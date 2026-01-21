830037792125c4d0bf55dc97480bc3e089ecc785
Tomasz Straczek <tomasz1.straczek@uj.edu.pl>
2026-01-21 12:01:23 +0100

    code style changes

--- src/pkoffee/tests/test_imports.py
+++ src/pkoffee/tests/test_imports.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytest
 
+
 def test_size_mismatch_true():
     from pkoffee.metrics import check_size_match
 
@@ -9,11 +10,12 @@ def test_size_mismatch_true():
 
     return check_size_match(a, b)
 
+
 def test_size_mismatch_false():
     from pkoffee.metrics import check_size_match, SizeMismatchError
+
     a = np.array([1, 2, 3])
     b = np.array([4, 5, 6, 8])
 
     with pytest.raises(expected_exception=SizeMismatchError):
         check_size_match(a, b)
-
